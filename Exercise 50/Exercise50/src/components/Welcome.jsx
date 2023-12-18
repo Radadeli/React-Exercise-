a1f67388b18b4bc49f656a2284592fa3ad6c96e7
Radadeli <ramirodelillo@gmail.com>
2023-12-18 15:14:34 +0100

    Exercise 57 Conditional

--- Exercise 50/Exercise50/src/components/Welcome.jsx
+++ Exercise 50/Exercise50/src/components/Welcome.jsx
@@ -3,7 +3,11 @@ export function Welcome({ name, age }) {
   return (
     <div>
     <p>Welcome, <strong>{name}</strong> </p>
+    {age > 18 && <Age age = {age} />}
+    {Boolean (age) && <Age age = {age} />}
+    {age > 18 && age < 65 && <Age age = {age} />}
     <Age age = {age} />
+    {age > 18 && age < 65 && name === 'Jhon' && <Age age = {age} />}
     </div>
   );
 }
