9e22334020c8607aeeba0813827ee81e688de180
Radadeli <ramirodelillo@gmail.com>
2023-12-18 14:29:25 +0100

    Exercise 53

--- Exercise 50/Exercise50/src/components/App.jsx
+++ Exercise 50/Exercise50/src/components/App.jsx
@@ -5,10 +5,14 @@ const App = () => {
     return (
         <div>
         <div> <Hello /> </div>
+        <div> <Hello /> </div>
+        <div> <Hello /> </div>
         <div> <Message /> </div>
         </div>
     )
 
 }
 
+// Se puede utilizar el componente 'Hello' más de una vez sin tener problemas. 
+// El componente Message se puede renderizar sin problemas 
 export default App; 
