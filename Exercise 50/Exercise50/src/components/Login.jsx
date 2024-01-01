c6293482477a60d03d3bc94776153050dfd24d58
Radadeli <ramirodelillo@gmail.com>
2024-01-01 22:21:08 +0100

    Exercise 66

--- Exercise 50/Exercise50/src/components/Login.jsx
+++ Exercise 50/Exercise50/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export function Login() {
+export function Login({ onLogin }) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [remember, setRemember] = useState('');
@@ -15,11 +15,16 @@ export function Login() {
    const handleRememberChange = (event) => {
             setRemember(event.target.checked);
    }
+   const handleLoginClick = () => {
+    onLogin({ username, password, remember})
+}
+    const isDisabled = username.trim() === '' || password.trim() === ''
     return(
         <div>
             <input name="username" value={username} onChange={handleUsernameChange} />
             <input name="password" type="password" value={password} onChange={handlePasswordChange}/>
             <input type="checkbox" checked={remember} onChange={handleRememberChange} />
+            <button onClick={handleLoginClick} disabled= {isDisabled}> Login</button>
         </div>
     )
 }
