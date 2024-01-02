0a964d8cfe2b362b0e8ee479029e4ca3339dee82
Radadeli <ramirodelillo@gmail.com>
2024-01-02 03:15:20 +0100

    correcciones

--- Exercise 50/Exercise50/src/components/Login.jsx
+++ Exercise 50/Exercise50/src/components/Login.jsx
@@ -1,44 +1,55 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export function Login({ onLogin }) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [remember, setRemember] = useState('');
-    const [reset, setReset] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState("");
 
-    const handleUsernameChange = (event) => {
-        setUsername(event.target.value);
-    }
-        const handlePasswordChange = (event) => {
-            setPassword(event.target.value);
-        }
-   const handleRememberChange = (event) => {
-            setRemember(event.target.checked);
-   }
-   const handleLoginClick = () => {
-    onLogin({ username, password, remember})
-}
-const handleLogin = (event) => {
-    event.preventDefault();
 
-   
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleRememberChange = (event) => {
+    setRemember(event.target.checked);
+  };
+  const handleLoginClick = () => {
     onLogin({ username, password, remember });
+  };
+  const handleLogin = (event) => {
+    event.preventDefault();
+    onLogin({ username, password, remember });
+  };
+  const handleResetForm = () => {
+    setUsername("");
+    setPassword("");
+    setRemember("");
+  };
+
+  const isDisabled = username.trim() === "" || password.trim() === "";
+  return (
+    <form onSubmit={handleLogin}>
+      <input name="username" value={username} onChange={handleUsernameChange} />
+      <input
+        name="password"
+        type="password"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      <input
+        type="checkbox"
+        checked={remember}
+        onChange={handleRememberChange}
+      />
+      <button onClick={handleLoginClick} disabled={isDisabled}>
+        {" "}
+        Login
+      </button>
+      <button onClick={handleResetForm}> Reset </button>
+    </form>
+  );
 }
-    const handleResetForm = () => {
-        setUsername('');
-        setPassword('');
-        setRemember('');
-    }
-    
-    const isDisabled = username.trim() === '' || password.trim() === ''
-    return(
-        <div>
-            <form onSubmit={handleLogin}> </form>
-            <input name="username" value={username} onChange={handleUsernameChange} />
-            <input name="password" type="password" value={password} onChange={handlePasswordChange}/>
-            <input type="checkbox" checked={remember} onChange={handleRememberChange} />
-            <button onClick={handleLoginClick} disabled= {isDisabled}> Login</button>
-            <button onClick={handleResetForm}> Reset </button>
-        </div>
-    )
-}
+
+export default Login; 
