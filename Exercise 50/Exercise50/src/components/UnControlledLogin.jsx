2e0d381d29de2cdb8ddbd64d90e731c08d4c6c24
Radadeli <ramirodelillo@gmail.com>
2024-01-02 15:18:12 +0100

    Exercise 70

--- Exercise 50/Exercise50/src/components/UnControlledLogin.jsx
+++ Exercise 50/Exercise50/src/components/UnControlledLogin.jsx
@@ -2,14 +2,15 @@ export function UnControlledLogin() {
  function handleFormSubmit (event){
     event.preventDefault()
  
-    const username = event.target.elements.namedItem('username').value
-    const password = event.target.elements.namedItem('password').value
-    const session = event.target.elements.namedItem('session').checked
+    // const username = event.target.elements.namedItem('username').value
+    // const password = event.target.elements.namedItem('password').value
+    // const session = event.target.elements.namedItem('session').checked
 
+        const formData = new formData(event.target)
     const data = {
-        username,
-        password,
-        session
+        username: formData.get('username'),
+        password: formData.get('password'),
+        session: formData.get('session') === 'on' ? true : false,
     }
     console.log(data)
 }
