b901efec9019f14afb815ecee42629af95371ad4
Radadeli <ramirodelillo@gmail.com>
2024-01-17 21:18:44 +0100

    cambios

--- Exercise 50/Exercise50/src/components/App.jsx
+++ Exercise 50/Exercise50/src/components/App.jsx
@@ -1,114 +1,29 @@
-import Hello from "./Hello";
-import Message from "./Message";
 import Welcome from "./Welcome";
-import AlertClock from "./AlertClock";
+
 import Counter from "./Counter";
-import Clock from "./Clock";
-import { MouseClicker } from "./MouseClicker";
-import InteractiveWelcome from "./InteractiveWelcome";
-import { Login } from "./Login";
-import { UnControlledLogin } from "./UnControlledLogin";
-import FocusableInput from "./FocusableInput";
-import { UseEffectComponent } from "./UseRefComponent";
-import { Colors } from "./Colors";
-import ToDoList from "./ToDoList";
-import Container from "./Container";
-import { LanguageContext } from "./LanguageContext";
-import { useState } from "react";
-import { GitHubUsers } from "./GitHubUsers";
-import { CounterHook } from "./CounterHook";
+
+import { Routes, Route, Link } from "react-router-dom";
 
 
 const App = () => {
-  const [language, setLanguage] = useState("en");
-  function handleSetLanguage(language) {
-    setLanguage(language);
-  }
+  
+    
   return (
     <div>
-      <button style={{ width: "30px" }} onClick={() => handleSetLanguage("en")}>
-        ES
-      </button>
-      <button onClick={() => handleSetLanguage("es")}>EN</button>
-      <Container title={<h1>The best application</h1>}>
-        <LanguageContext.Provider value={language}>
-          <Clock />
-        </LanguageContext.Provider>
-        <div>
-          {" "}
-          <Hello />{" "}
-        </div>
-        <div>
-          {" "}
-          <Hello />{" "}
-        </div>
-        <div>
-          {" "}
-          <Hello />{" "}
-        </div>
-        <br />
-        <div>
-          {" "}
-          <Message />{" "}
-        </div>
-        <br />
-        <div>
-          {" "}
-          <Welcome name="Ramiro" age={25} />{" "}
-        </div>
-        <br />
-        <div>
-          <AlertClock />
-        </div>
-        <br />
-        <div>
-          <Counter />
-        </div>
-        <br />
-        <div>
-          <MouseClicker />
-        </div>
-        <br />
+      <nav>
+    <Link to= "/Welcome"> Welcome Exercise</Link>  
+    <Link to = "/Counter">Counter Exercise</Link>
+    </nav>
+    <Routes>
+    <Route path="/Welcome" element={<Welcome name={"Ramiro"} age={28}/>}/>
+    <Route path="/Counter" element={<Counter/>}/>
+   
 
-        <div>
-          <InteractiveWelcome />
-        </div>
-        <br />
-        <div>
-          <Login />
-        </div>
-        <hr />
-        <br />
-        <div>
-          <UnControlledLogin />
-        </div>
-        <br />
-
-        <div>
-          <FocusableInput />
-        </div>
-        <div>
-          <UseEffectComponent />
-        </div>
-        <div>
-          <Colors />
-        </div>
-        <div>
-          <ToDoList />
-        </div>
-        
-      </Container>
-      <br />
-      <div>
-      <hr />
-      <br />
-        <GitHubUsers/>
-        <hr />
-      </div>
-      <CounterHook/>
-
-      
+    </Routes>
     </div>
+
+    
+ 
   );
 };
 // Exercise 52
@@ -116,3 +31,11 @@ const App = () => {
 // El componente Message se puede renderizar sin problemas
 
 export default App;
+    {/* <nav>
+      <Link to="/">Home page</Link> | <Link to="/counter">Counter</Link>
+      <hr />
+    </nav>
+      <Routes>
+        <Route path="/" element={<Welcome name="Ramiro" />} />
+        <Route path="/counter" element={<Counter initialValue={0} difAmount={5} />}/>
+      </Routes> */}
